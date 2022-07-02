46fc67861962ad04a1bd228ba3f5f16f8db37e57
Emrullah <emr.ozk.71@gmail.com>
2022-07-02 23:28:07 +0300

    First commit

---
+++ src/main/java/com/emr/producter/ProducterApplication.java
@@ -0,0 +1,13 @@
+package com.emr.producter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emr/producter/ProducterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.producter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
